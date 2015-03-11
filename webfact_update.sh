dd211642c3bef16faa81664b0eef8d8a9dd649ab
Sean Boran <sean.boran@swisscom.com>
2015-03-11 12:54:57 +0000

    webfact_update.sh is for specifically an example for updating a webfactory UI

--- webfact_update.sh
+++ webfact_update.sh
@@ -5,25 +5,20 @@
 # ln -s /var/www/html/sites/all/modules/custom/webfact/webfact_update.sh /var/www/html/webfact_update.sh
 # or just copying into /var/www/html. Make sure it is executable.
 #
+# This example updated /var/www/html and all webfact modules from git.
 
-echo "Running webfact_update.sh";
+echo "Running webfact_update.sh: updating elements of a Webfactory UI";
 cd /var/www/html
 if [ -d '.git' ] ; then
   echo "-- todo: dump the db and files to /data (or other external volume)"
   if [ -x /root/backup.sh ] ; then
     echo "-- backup: running /root/backup.sh /data"
   fi;
-  #echo "-- todo: dump the db and files to /data (or other external volume)"
 
-
-  echo "-- git pull"
+  echo "-- git pull for /var/www"
+  #echo "todo: how to use save/use credientials for git? "
   git pull
-  echo "todo: how to use save/use credientials for git? "
 
-  echo " "
-  echo "-- Drush update, clear caches"
-  drush updatedb
-  drush cache-clear all
 
   echo " "
   echo "-- last two git logs:"
@@ -31,4 +26,19 @@ if [ -d '.git' ] ; then
 else
   echo "no git repo in /var/www/html";
 fi
+
+
+echo "-- git pull for webfact components: module, theme, webfact_content_types, webfactapi:"
+(cd sites/all/modules/custom/webfact && git pull)
+(cd sites/all/themes/webfact_theme && git pull)
+(cd sites/all/modules/custom/webfact_content_types/ && git pull)
+(cd sites/all/modules/custom/webfactapi && git pull)
+
+
+# finally:
+echo " "
+echo "-- Drush update, clear caches"
+drush updatedb
+drush cache-clear all
+echo "-- webfact_update.sh done --"
 

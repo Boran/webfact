1967cc0d5d55b8bcbd00eadf4c83d25e2bc5cc93
Sean Boran <sean.boran@swisscom.com>
2015-03-25 13:01:33 +0000

    specific status for webfact

--- webfact_status.sh
+++ webfact_status.sh
@@ -6,13 +6,29 @@
 # or just copying into /var/www/html. Make sur it is executable.
 
 cd /var/www/html
-if [ -d '.git' ] ; then
-  lastcommit=` git log -n 1 --date=short --abbrev-commit --pretty=oneline 2>&1 `;
+
+## generic example:
+#if [ -d '.git' ] ; then
+  lastcommit=`git log -n 1 --date=short --abbrev-commit --pretty=oneline 2>&1 `;
   remote=`git remote -v 2>&1|grep fetch|awk '{print $2}' `
   #changes=`git status -s -b 2>&1 `
   #echo "Last commit: '$lastcommit', remote=$remote, changes=$branch";
-  echo "Last commit: '$lastcommit', remote=$remote";
-else
-  echo "no git repo in /var/www/html";
+  #3echo "Last commit: '$lastcommit', remote=$remote";
+
+  # date of last commit
+  #git log -n 1 --date=short|grep Date| awk '{print $2}'
+#else
+#  echo "no git repo in /var/www/html";
+#fi
+
+# For webafctory itself:
+echo "Last commits: "
+if [ -d /var/www/html/sites/all/modules/custom/webfact ] ; then
+  cd /var/www/html/sites/all/modules/custom/webfact
+  echo "webfact=`git log -n 1 --date=short|grep Date| awk '{print $2}'`"
+  cd /var/www/html/sites/all/modules/custom/webfact_content_types
+  echo ", webfact_content_types=`git log -n 1 --date=short|grep Date| awk '{print $2}'`"
+  cd /var/www/html/sites/all/modules/custom/webfactapi
+  echo ", webfactapi=`git log -n 1 --date=short|grep Date| awk '{print $2}'`"
 fi
 

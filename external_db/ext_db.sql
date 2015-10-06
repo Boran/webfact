cbd7be54c6d6b6dd57dade3ea9dca80ff9db1b70
boran <sean@boran.com>
2015-10-06 09:45:48 +0000

    RenameAppDB

--- external_db/ext_db.sql
+++ external_db/ext_db.sql
@@ -5,11 +5,10 @@
 #
 # Original snippit: http://superuser.com/questions/424476/grant-mysql-user-ability-to-create-databases-and-only-allow-them-to-access-thos#424493
 #
-# Install: first load the proc:
-#  mysql mysql < scripts/ext_db.sql
-# Then create and grant access to your webfact user
+# Create and grant access to your webfact user
 #   echo "create user webfact_create identified by 'ho4iaH4o'" | mysql mysql
-#   echo "grant execute on procedure CreateAppDB to 'webfact_create'@'%'" | mysql mysql
+# Install the procedures:
+#  mysql mysql < external_db/ext_db.sql
 # then
 # add that user/pw to the settings in the webfact UI, admin/config/development/webfact.
 #
@@ -18,10 +17,6 @@ DELIMITER //
 DROP PROCEDURE IF EXISTS CreateAppDB//
 CREATE PROCEDURE CreateAppDB(IN db_name VARCHAR(50), IN db_user VARCHAR(50), IN db_pw VARCHAR(50))
 BEGIN
-    #SET @s = CONCAT('CREATE USER ', db_user, ' IDENTIFIED BY ''', db_pw, '''');
-    #PREPARE stmt FROM @s;
-    #EXECUTE stmt;
-    #DEALLOCATE PREPARE stmt;
     # grant will do a create user if needed
     SET @s = CONCAT('GRANT ALL ON ', db_name, '.* TO ''', db_user, '''@''%''' , ' IDENTIFIED BY ''', db_pw, '''');
     PREPARE stmt FROM @s;
@@ -51,32 +46,27 @@ BEGIN
     DEALLOCATE PREPARE stmt;
 END//
 
-DELIMITER ;
-grant execute on procedure CreateAppDB to 'webfact_create'@'%';
-grant execute on procedure DeleteAppDB to 'webfact_create'@'%';
 
 -- stored procedure rename_db: Rename a database my means of table copying.
--- http://stackoverflow.com/questions/67093/how-do-i-quickly-rename-a-mysql-database-change-schema-name
--- Caveats: 
 -- Will clobber any existing database with the same name as the 'new' database name.
--- ONLY copies tables; stored procedures, views, triggers and other db objects are not copied.
--- Tomer Altman (taltman@ai.sri.com)
+-- Only copies tables; stored procedures, views, triggers and other db objects are not copied.
+-- Tomer Altman (taltman@ai.sri.com) http://stackoverflow.com/questions/67093/how-do-i-quickly-rename-a-mysql-database-change-schema-name
 -- Webfactory: We call it RenameAppDB() rather than rename_db(). Adapted to rename user too.
+
 
 delimiter //
 DROP PROCEDURE IF EXISTS RenameAppDB;
-CREATE PROCEDURE RenameAppDB(IN old_db VARCHAR(100), IN new_db VARCHAR(100), IN old_user VARCHAR(50), new_user VARCHAR(5))
+CREATE PROCEDURE RenameAppDB(IN old_db VARCHAR(100), IN new_db VARCHAR(100), IN old_user VARCHAR(50), new_user VARCHAR(50))
 BEGIN
     DECLARE current_table VARCHAR(100);
     DECLARE done INT DEFAULT 0;
     DECLARE old_tables CURSOR FOR select table_name from information_schema.tables where table_schema = old_db;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 
-    # Delete and create the newdb
-    SET @output = CONCAT('DROP SCHEMA IF EXISTS ', new_db, ';'); 
-    PREPARE stmt FROM @output;
-    EXECUTE stmt;
-
+    # Delete and create the newdb, but delete not needed for webfactory?
+    #SET @output = CONCAT('DROP SCHEMA IF EXISTS ', new_db, ';'); 
+    #PREPARE stmt FROM @output;
+    #EXECUTE stmt;
     SET @output = CONCAT('CREATE SCHEMA IF NOT EXISTS ', new_db, ';');
     PREPARE stmt FROM @output;
     EXECUTE stmt;
@@ -94,9 +84,18 @@ BEGIN
     UNTIL done END REPEAT;
     CLOSE old_tables;
     
-    ## todo: drop olddb
+    ## olddb
+    SET @output = CONCAT('DROP SCHEMA IF EXISTS ', old_db, ';'); 
+    PREPARE stmt FROM @output;
+    EXECUTE stmt;
+
     ## rename user too
-    SET @output = CONCAT('RENAME USER ', old_user, ' ' , new_user , ';'); 
+    SET @output = CONCAT('RENAME USER ', old_user, ' TO ' , new_user , ';'); 
     PREPARE stmt FROM @output;
     EXECUTE stmt;
 END//
+DELIMITER ;
+grant execute on procedure CreateAppDB to 'webfact_create'@'%';
+grant execute on procedure DeleteAppDB to 'webfact_create'@'%';
+grant execute on procedure RenameAppDB to 'webfact_create'@'%';
+show procedure status;
